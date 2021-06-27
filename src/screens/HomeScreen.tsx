3a2b5a1bb9b6b2bd960a2b8a0faf3fa135f4eda6
adriton <adrtler@gmail.com>
2021-06-27 12:26:15 +0200

    Tipado MovieDB con quicktype

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,12 +2,15 @@ import {StackScreenProps} from '@react-navigation/stack';
 import React, {useEffect} from 'react';
 import {Button, Text, View} from 'react-native';
 import movieDB from '../api/movieDB';
+import {MovieDBNowPlaying} from '../interfaces/movie.interface';
 
 interface Props extends StackScreenProps<any, any> {}
 
 export const HomeScreen = ({navigation: {navigate}}: Props) => {
   useEffect(() => {
-    movieDB.get('/now_playing').then(res => console.log(res));
+    movieDB
+      .get<MovieDBNowPlaying>('/now_playing')
+      .then(res => console.log(res.data.results[0].original_title));
   }, []);
   return (
     <View>
