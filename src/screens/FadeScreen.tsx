4f8ecd2da1ba9e6da796f23febb00a00d6d326cb
adriton <adrtler@gmail.com>
2021-07-14 17:48:38 +0200

    Hook de fadeIn, fadeOut

--- src/screens/FadeScreen.tsx
+++ src/screens/FadeScreen.tsx
@@ -1,22 +1,9 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import {Text, View, Animated, Button} from 'react-native';
+import {useFade} from '../hooks/useFade';
 
 export const FadeScreen = () => {
-  const opacity = useRef(new Animated.Value(0)).current;
-  const fadeIn = () => {
-    Animated.timing(opacity, {
-      toValue: 1,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
-  };
-  const fadeOut = () => {
-    Animated.timing(opacity, {
-      toValue: 0,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
-  };
+  const {opacity, fadeIn, fadeOut} = useFade();
   return (
     <View
       style={{
